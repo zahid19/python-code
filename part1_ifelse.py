6a6aa4a878dcd80168a6f73558e5d47f5d849488
Zahid Bin Abdur Rouf <zahid19@gmail.com>
2020-10-30 18:28:57 +0600

    Create part1_ifelse.py

---
+++ part1_ifelse.py
@@ -0,0 +1,12 @@
+# PYTHON IF-ELSE CONDITION PRACTICE CODE
+# INPUT: MARK=40  # OUTPUT: No grade found
+
+mark=40
+if mark >= 80:
+print("A+")
+print("Excellent")
+elif mark >=75 and mark <=79:
+print("A")
+print("Excellent")
+else:
+print("No grade found")
