d7efe8e410d7c0bccd19b1b93697379fcdbf8902
Zahid Rouf <zahid19@gmail.com>
2020-11-03 01:27:15 +0600

    Update classobjectmethod.py

--- classobjectmethod.py
+++ classobjectmethod.py
@@ -1,20 +1,11 @@
-# Problem Statement: Write a Python class which take two methods
-# get_String and print_String
-# get_String accept a string from the user and store the string as upper case
-# then print_String print the string in lower case
-# Objective: 
-# To take input from user and store input in uppercase format. Print stored input in lowercase format
-# Platform: Visual Studio (ver 1.46)
-# Purpose: Stores input in Uppercase,Prints input in lowercase
-
 class Python:
- string = ''
+ stringName = ""
  def get_String(self,userinput):
-  self.string = userinput.upper()
+  self.stringName = userinput.upper()
  def print_String(self):
-  print(self.string.lower())
+  print(self.stringName.lower())
 
-p1 = Python( )
+p1 = Python()
 name = input()
 p1.get_String(name)
 p1.print_String()
